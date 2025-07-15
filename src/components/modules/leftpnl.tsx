5660e5a9da4866b743fd9c8f0c94eec7f5ba56a1
noguri98 <rbals46209221923@gmail.com>
2025-07-15 10:31:12 +0900

    style: leftpnl 책임 분리

--- src/components/modules/leftpnl.tsx
+++ src/components/modules/leftpnl.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { getJsonData, getDateTime } from "@/utils/funLeftpnl";
-import { useState, useEffect } from "react";
+import { getJsonData } from "@/utils/funLeftpnl";
+import { useDateTimeUpdate } from "@/hooks/useLeftpnl";
 
 import Profile from "@/components/com/leftpnl_profile";
 import DateTime from "@/components/com/leftpnl_datetime";
@@ -22,20 +22,11 @@ export default function LeftPnl() {
         }
     */
 
-    // 시간 정보를 state로 관리
-    const [dateTimeInfo, setDateTimeInfo] = useState(() => 
-        getDateTime(jsonData.info.nation, jsonData.info.city)
-    );
-
-    // 1초마다 시간 업데이트
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setDateTimeInfo(getDateTime(jsonData.info.nation, jsonData.info.city));
-        }, 1000);
-
-        // 컴포넌트 언마운트 시 타이머 정리
-        return () => clearInterval(timer);
-    }, [jsonData.info.nation, jsonData.info.city]);
+    // 커스텀 훅을 사용하여 시간 정보 관리
+    const { dateTimeInfo } = useDateTimeUpdate({
+        nation: jsonData.info.nation,
+        city: jsonData.info.city
+    });
 
     return (
         <div className='flex items-center flex-col border-1 border-gray-300 rounded-[20px] gap-[30px] px-2 pb-4' style={{ width: '250px', minHeight: '500px', height: '100%'}}>
